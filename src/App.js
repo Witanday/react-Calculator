7adf01fb895a9f826e19fc8f92e686d8c7396021
Witanday <witanday.cd@gmail.com>
2018-08-02 23:01:17 -0400

    working on state'
    ls
    mnhhjio

--- src/App.js
+++ src/App.js
@@ -4,36 +4,56 @@ import './App.css';
 
 
 class App extends Component {
-
-  constructor(){
-    super();
-    this.state ={
-      expression:'0'
+    state ={               
+      displayValue:'0'
     }
+    clearDisplay(digit) {
+      this.setState({
+        displayValue:  '0' 
+      })
+    }
+    inputDigit(digit) {
+      const {displayValue}=this.state;
+
+      this.setState({
+        displayValue: displayValue === '0' ? String(digit) : displayValue + digit
+      })
+    }
+      inputDot() {
+        const {displayValue}=this.state;
+        
+        if (displayValue.lastIndexOf('.') === -1){
+          this.setState({
+            displayValue: displayValue + '.'
+          })
+        
+        }
+     
   }
   render() {
+    const {displayValue}=this.state;
     return (
       <div className="App">
-      <div className="calcDisplay">0</div>
+      <div className="calcDisplay">{displayValue}</div>
         <div className="keypad">
           <div className="inputKey">
             <div className="functionKey">
-              <button className="appKey1 key">AC</button>
-              <button className="appKey1 key">AC</button>
-              <button className="appKey1 key">AC</button>
+              <button className="appKey1 key" onClick={() => this. clearDisplay ()}>C</button>
+              <button className="appKey1 key">+/-</button>
+              <button className="appKey1 key">%</button>
             </div>
             <div className="digitKey">
-              <button className="appKey2 key">7</button>
-              <button className="appKey2 key">8</button>
-              <button className="appKey2 key">9</button>
-              <button className="appKey2 key">4</button>
-              <button className="appKey2 key">5</button>
-              <button className="appKey2 key">6</button>
-              <button className="appKey2 key">1</button>
-              <button className="appKey2 key">2</button>
-              <button className="appKey2 key">3</button>
-              <button className="appKey2 key zero">0</button>
-              <button className="appKey2 key">.</button>
+              <button className="appKey2 key" onClick={() => this.inputDigit(7)} >7</button>
+              <button className="appKey2 key" onClick={() => this.inputDigit(8)} >8</button>
+              <button className="appKey2 key" onClick={() => this.inputDigit(9)} >9</button>
+              <button className="appKey2 key" onClick={() => this.inputDigit(4)} >4</button>
+              <button className="appKey2 key" onClick={() => this.inputDigit(5)} >5</button>
+              <button className="appKey2 key" onClick={() => this.inputDigit(6)} >6</button>
+              <button className="appKey2 key" onClick={() => this.inputDigit(1)} >1</button>
+              <button className="appKey2 key" onClick={() => this.inputDigit(2)} >2</button>
+              <button className="appKey2 key" onClick={() => this.inputDigit(3)} >3</button>
+              <button className="appKey2 key zero" onClick={() => this.inputDigit(0)} >0</button>
+              <button className="appKey2 key" onClick={() => this.inputDot()} >. </button>
             </div>
           </div>
           <div className="operatorKey">
